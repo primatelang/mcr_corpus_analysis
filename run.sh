c48e0c644a1bffb8e50269ad106af528e1a71e5c
Juan Benjumea <jubenjum@gmail.com>
2018-01-11 16:50:55 +0100

    removed from run.sh corpus that are not used

--- run.sh
+++ run.sh
@@ -13,18 +13,6 @@ function run_pipeline {
     ./abx_pipeline.sh "$exp_name" "$input_dir" "$output_dir" 
 }
 
-###echo "...Putty Nosed..."
-###input_dir=./corpus/Putty_nosed 
-###output_dir=./outputs/Putty_nosed/
-###run_pipeline $input_dir $output_dir
-###
-###
-###echo "...Campbell's monkey..."
-###input_dir=./corpus/Campbell_Tiwai_recordings
-###output_dir=./outputs/Campbell_Tiwai_recordings
-###run_pipeline $input_dir $output_dir
-###
-###
 ###echo "...Blue monkey..."
 ###input_dir=./corpus/Blue_monkey/Murphy 
 ###output_dir=././outputs/Blue_monkey/Murphy/
@@ -38,31 +26,3 @@ input_dir=./corpus/Blue_monkey/Join
 output_dir=././outputs/Blue_monkey/Join
 run_pipeline $input_dir $output_dir
 
-###
-###
-###echo "...Colobus guereza..."
-###input_dir=./corpus/Colobus_guereza
-###output_dir=./outputs/Colobus_guereza/
-###run_pipeline $input_dir $output_dir
-###
-###
-###echo "...Titi..."
-###input_dir=./corpus/Titi_monkey/Non-predator_sequences 
-###output_dir=./outputs/Titi_monkey/Non-predator_sequences/
-###run_pipeline $input_dir $output_dir
-###
-###input_dir=./corpus/Titi_monkey/Raptor_model_canopy
-###output_dir=./outputs/Titi_monkey/Raptor_model_canopy/
-###run_pipeline $input_dir $output_dir
-###
-###
-###
-#### Gibbons
-###
-### NOTE: Gibbons' calls are repeted all time, there number of calls is high
-### and that does make abx collapse (too many triplets)
-###input_dir=./corpus/Gibbons
-###output_dir=./outputs/Gibbons/
-###mkdir -p $output_dir
-###cp /home/juan/projects/mcr/src/segmented.cfg "$output_dir"
-###run_pipeline.sh "$input_dir" "$output_dir"
